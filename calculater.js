ea04a7072b828d977a79f050861b0a5ea3e3dbcd
ikulsaini <112651147+ikulsaini@users.noreply.github.com>
2022-09-20 12:10:33 +0530

    Add files via upload

---
+++ calculater.js
@@ -0,0 +1,24 @@
+var num = parseInt(prompt("enter the day number :" ))
+if(num==1)
+document.write(`'${num}' is Monday`)
+else if(num==2){
+    document.write(`'${num}' is Tuesday `)
+}
+else if(num==3){
+    document.write(`'${num}' is , Wednesday `)
+}
+else if(num==4){
+    document.write(`'${num}' is Thursday `)
+}
+else if(num==5){
+    document.write(`'${num}' is  Friday `)
+}
+else if(num==6){
+    document.write(`'${num}' is Saturday `)
+}
+
+ if(num==7){
+    document.write(`'${num}' is Sunday`)
+}
+
+   
